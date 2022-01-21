8d0eb283fa9311b15d7095e2f367d6cb1a02801a
jakubdziuba23 <waldes234@gmail.com>
2022-01-21 20:24:58 +0100

    Utworzenie modelu konta użytkownika

--- Forum/Forum/urls.py
+++ Forum/Forum/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('account/', include('register.urls')),
     path('hitcount/', include('hitcount.urls', namespace='hitcount')),
 ]
--- Forum/main/views.py
+++ Forum/main/views.py
@@ -1,8 +1,11 @@
 from multiprocessing import context
 from unicodedata import category
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Author, Category, Post
 from .utils import update_views
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
     forums = Category.objects.all()
@@ -29,3 +32,22 @@ def posts(request, slug):
     }
 
     return render(request, "posts.html", context)
+
+
+@login_required
+def create_post(request):
+    context = {}
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            print("\n\n Nieprawidłowe")
+            author = Author.objects.get(user=request.user)
+            new_post = form.save(commit=False)
+            new_post.user = author
+            new_post.save()
+            return redirect("home")
+    context.update({
+        "form": form,
+        "title": "Create New Post"
+    })
+    return render(request, "create_post.html", context)
