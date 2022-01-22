d84f7664fa49c06744d61cd42fb9a6d871680741
JanPaJ <patryk4857@gmail.com>
2022-01-22 23:33:05 +0100

    Komentarze i odpowiedzi
    Signed-off-by: JanPaJ <patryk4857@gmail.com>

--- Forum/main/urls.py
+++ Forum/main/urls.py
@@ -4,8 +4,8 @@ from .views import home, detail, posts, create_post, latest_posts
 
 urlpatterns = [
     path("", home, name="home"),
-    path("detail/<slug>", detail, name="detail"),
-    path("posts/<slug>", posts, name="posts"),
+    path("detail/<slug>/", detail, name="detail"),
+    path("posts/<slug>/", posts, name="posts"),
     path("create_post/", create_post, name="create_post"),
     path("latest_posts", latest_posts, name="latest_posts"),
 ]
