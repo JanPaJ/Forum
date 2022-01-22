83e8ca5120de0b8d4c146c7faebcbadf555ac9ee
jakubdziuba23 <waldes234@gmail.com>
2022-01-22 10:59:23 +0100

    Dodanie statystyk dla serwisu

--- Forum/main/urls.py
+++ Forum/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, detail, posts, create_post
+from .views import home, detail, posts, create_post, latest_posts
 
 
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
     path("detail/<slug>", detail, name="detail"),
     path("posts/<slug>", posts, name="posts"),
     path("create_post/", create_post, name="create_post"),
-
+    path("latest_posts", latest_posts, name="latest_posts"),
 ]
--- Forum/main/views.py
+++ Forum/main/views.py
@@ -5,30 +5,81 @@ from .models import Author, Category, Post
 from .utils import update_views
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def home(request):
     forums = Category.objects.all()
+    num_posts = Post.objects.all().count()
+    num_users = User.objects.all().count()
+    num_categories = forums.count()
+    try:
+        last_post = Post.objects.latest("date")
+    except:
+        last_post = []
+
     context = {
-        "forums": forums
+        "forums":forums,
+        "num_posts":num_posts,
+        "num_users":num_users,
+        "num_categories":num_categories,
+        "last_post":last_post,
     }
     return render(request, "home.html", context)
 
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+
+    forums = Category.objects.all()
+    num_posts = Post.objects.all().count()
+    num_users = User.objects.all().count()
+    num_categories = forums.count()
+    try:
+        last_post = Post.objects.latest("date")
+    except:
+        last_post = []
+
     context = {
         "post":post,
+        "forums":forums,
+        "num_posts":num_posts,
+        "num_users":num_users,
+        "num_categories":num_categories,
+        "last_post":last_post,
     }
     update_views(request, post)
     return render(request, "detail.html", context)
 
 def posts(request, slug):
+
+    forums = Category.objects.all()
+    num_posts = Post.objects.all().count()
+    num_users = User.objects.all().count()
+    num_categories = forums.count()
+    try:
+        last_post = Post.objects.latest("date")
+    except:
+        last_post = []
+
+
     category = get_object_or_404(Category, slug=slug)
     posts = Post.objects.filter(approved = True, categories = category)
-
+    paginator = Paginator(posts, 5)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages) 
     context = {
         "posts":posts,
         "forum":category,
+        "num_posts":num_posts,
+        "num_users":num_users,
+        "num_categories":num_categories,
+        "last_post":last_post,
     }
 
     return render(request, "posts.html", context)
@@ -51,3 +102,13 @@ def create_post(request):
         "title": "Create New Post"
     })
     return render(request, "create_post.html", context)
+
+
+def latest_posts(request):
+    posts = Post.objects.all().filter(approved=True)[:10]
+    context = {
+        "posts":posts,
+        "title": "OZONE: Latest 10 Posts"
+    }
+
+    return render(request, "latest_posts.html", context)
